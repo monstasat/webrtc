61258b36d6c5ef7ceb5f98da0c2d8ca46008acbe
alexander <sashayanin@gmail.com>
2017-05-04 22:56:45 +0300

    changed to vp8

--- gst/rtp.sh
+++ gst/rtp.sh
@@ -5,7 +5,7 @@ gst-launch-1.0 \
     opusenc bitrate=20000 ! \
       rtpopuspay ! udpsink host=127.0.0.1 port=5002 \
   videotestsrc ! \
-    video/x-raw,width=1920,height=1080,framerate=25/1 ! \
+    video/x-raw,width=640,height=480,framerate=25/1 ! \
     videoscale ! videorate ! videoconvert ! timeoverlay ! \
-    x264enc ! \
-    rtph264pay pt=96 ! udpsink host=127.0.0.1 port=5004
+    vp8enc ! \
+    rtpvp8pay ! udpsink host=127.0.0.1 port=5004
