001ce24102056b7e379527102348db4abd055753
alexander <sashayanin@gmail.com>
2017-05-17 15:48:37 +0300

    some comments added

--- ats3_html/js/webrtc_streaming.js
+++ ats3_html/js/webrtc_streaming.js
@@ -36,12 +36,23 @@ window.onload = function() {
                             plugin: "janus.plugin.streaming",
                             opaqueId: opaqueId,
                             success: function(pluginHandle) {
+                                // Plugin attached! 'PluginHandle' is our handle
                                 streaming = pluginHandle;
                                 Janus.log("Streaming plugin attached! (" + streaming.getPlugin() + ", id=" + streaming.getId() + ")");
                                 updateStreamsList();
                             },
                             error: function(error) {
+                                // Could not attach to the plugin
                                 alert("Error attaching streaming plugin... " + error);
+                            },
+                            // This callback is triggered just before getUserMedia is called
+                            // (parameter=true) and after it completed (parameter=false);
+                            // this means it can be used to modify the UI accordingly, e.g.
+                            // to promt the user about the need to accept the device access
+                            // consent requests
+                            consentDialog: function(on) {
+                                // e.g. darken the screen if on=true (getUserMedia incoming),
+                                // restore it otherwise
                             },
                             onmessage: function(msg, jsep) {
                                 Janus.debug(" ::: Got a message :::");
@@ -62,32 +73,53 @@ window.onload = function() {
 										                stopStream();
 										                return;
 									              }
+                                // jsep - javascript session establishment protocol
+                                // if jsep is not null, message involves webrtc negotiation
 									              if(jsep !== undefined && jsep !== null) {
+                                    // we have an OFFER from the plugin
+                                    // sdp - session description protocol
 										                Janus.debug("Handling SDP as well...");
 										                Janus.debug(jsep);
 										                // Answer
 										                streaming.createAnswer(
 											                  {
+                                            // Attach the remote OFFER
 												                    jsep: jsep,
-												                    media: { audioSend: false, videoSend: false },	// We want recvonly audio/video
+                                            // We want recvonly audio/video
+												                    media: { audioSend: false, videoSend: false },
 												                    success: function(jsep) {
+                                                // Got our SDP! Send out ANSWER to the plugin
 													                      Janus.debug("Got SDP!");
 													                      Janus.debug(jsep);
 													                      var body = { "request": "start" };
 													                      streaming.send({"message": body, "jsep": jsep});
-													                      $('#watch').html("Stop").removeAttr('disabled').click(stopStream);
 												                    },
 												                    error: function(error) {
+                                                // An error occured
 													                      Janus.error("WebRTC error:", error);
 													                      bootbox.alert("WebRTC error... " + JSON.stringify(error));
 												                    }
 											                  });
 									              }
                             },
+                            onlocalstream: function(stream) {
+                                // This will NOT be invoked, we've choosen recvonly
+                            },
+                            // a remote media stream is available and ready to be displayed
                             onremotestream: function(stream) {
+                                // invoked after send has got us a PeerConnection
+                                // This is the remote video
                                 Janus.debug(" ::: Got a remote stream ::: ");
                                 Janus.debug(JSON.stringify(stream));
                                 Janus.attachMediaStream($('#remotevideo').get(0), stream);
+                            },
+                            oncleanup: function() {
+                                // PeerConnection with the plugin closed, clean the UI
+                                // The plugin handle is still valid so we can create a new one
+                            },
+                            detached: function() {
+                                // Connection with the plugin closed, get rid of its features
+                                // The plugin handle is not valid anymore
                             }
                         });
                 }
--- gst/test_gstreamer_1.sh
+++ gst/test_gstreamer_1.sh
@@ -3,9 +3,8 @@ gst-launch-1.0 \
   audiotestsrc ! \
     audioresample ! audio/x-raw,channels=1,rate=16000 ! \
     opusenc bitrate=20000 ! \
-      rtpopuspay ! udpsink host=127.0.0.1 port=5002 \
-  videotestsrc ! \
-    video/x-raw,width=720,height=576,framerate=25/1 ! \
-    videoscale ! videorate ! videoconvert ! timeoverlay ! \
-    vp8enc error-resilient=1 ! \
-      rtpvp8pay ! udpsink host=127.0.0.1 port=5004
+    rtpopuspay ! udpsink host=127.0.0.1 port=5002 \
+  filesrc location=/home/alexander/Videos/demo.mp4 ! \
+    qtdemux ! h264parse ! avdec_h264 ! \
+    videoscale ! video/x-raw,width=840,height=525 ! \
+    vp8enc ! rtpvp8pay ! udpsink host=127.0.0.1 port=5004
